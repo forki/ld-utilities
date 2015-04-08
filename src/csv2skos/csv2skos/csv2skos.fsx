045f4d9fe464fda280366d8afeb80ba657b0c887
Nathan Smith <nathan@smeef.co.uk>
2015-04-08 15:29:39 +0100

    tidy fsx script file

--- src/csv2skos/csv2skos/csv2skos.fsx
+++ src/csv2skos/csv2skos/csv2skos.fsx
@@ -1,72 +1,79 @@
-﻿#r "../packages/dotNetRDF/lib/net40/dotNetRDF.dll"
-#r "../packages/FSharp.RDF/lib/net40/FSharp.RDF.dll"
-#r "../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+﻿#r "../../../packages/dotNetRDF/lib/net40/dotNetRDF.dll"
+#r "../../../packages/FSharp.RDF/lib/net40/FSharp.RDF.dll"
+#r "../../../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+#r "../../../packages/VDS.Common/lib/net40-client/VDS.Common.dll"
+
+open VDS.Common
 open FSharp.Data
-open System
 open FSharp.RDF
 open Assertion
+open graph
 open rdf
 
-type skosv = CsvProvider<"/Users/Nate/MEGA/Development/FSharp/ld-ontologies/ontologies/ns/Sample Settings vocabulary to go in SKOS format.csv">
-let csv = skosv.Load("/Users/Nate/MEGA/Development/FSharp/ld-ontologies/ontologies/ns/Sample Settings vocabulary to go in SKOS format.csv")
 
-let mkTuple (r:skosv.Row) = (r.First,r.Second,r.Third)
+type csv = FSharp.Data.CsvProvider< Sample="First (string), Second (string), Third (string)" >
 
-let (|SeqEmpty|SeqCons|) (xs: 'a seq) =
-  if Seq.isEmpty xs then SeqEmpty
-  else SeqCons( Seq.head xs |> mkTuple, Seq.skip 1 xs)
+let loadCsv(p : string) = csv.Load(p)
+
+let rSpaces(x : string * string * string) =
+    match x with
+    | (x, y, z) ->
+        (x.Replace(' ', '-'), y.Replace(' ', '-'), z.Replace(' ', '-'))
+
+let mkTuple(r : csv.Row) = rSpaces(r.First, r.Second, r.Third)
+
+let (|SeqEmpty|SeqCons|)(xs : 'a seq) =
+    if Seq.isEmpty xs then SeqEmpty
+    else SeqCons(Seq.head xs |> mkTuple, Seq.skip 1 xs)
 
 let (|E|NE|) s =
-  if String.IsNullOrEmpty s then E
-  else NE s
+    if System.String.IsNullOrEmpty s then E
+    else NE s
 
-(**
-let rec toPaths xs p = [
-  let next p xs = p::(toPaths xs p)
-  match xs,p with
-  | SeqEmpty,_ -> ()
-  | SeqCons((NE v,E, E),xs),[] -> yield! next [v] xs
-  | SeqCons((NE v,E, E),xs),[a] -> yield! next [v] xs
-  | SeqCons((E,NE v, E),xs),[a] -> yield! next [v;a] xs
-  | SeqCons((E,NE v, E),xs),[a;b] -> yield! next [v;b] xs
-  | SeqCons((E,E, NE v),xs),[a;b] -> yield! next [v;a;b] xs
-  | SeqCons((E,E, NE v),xs),[a;b;c] -> yield! next [v;b;c] xs
-  | SeqCons((E, NE v, E),xs),[a;b;c] -> yield! next [v;c] xs
-  | SeqCons((NE v, E, E),xs), [a;b] -> yield! next [v] xs
-  | SeqCons(x,_),p -> printfn "%A %A" x p
- ]
+let rec toPaths xs p =
+    [let next p xs = p :: (toPaths xs p)
+     match xs, p with
+     | SeqEmpty, _ -> ()
+     | SeqCons((NE v, E, E), xs), [] -> yield! next [v] xs
+     | SeqCons((NE v, E, E), xs), [a] -> yield! next [v] xs
+     | SeqCons((E, NE v, E), xs), [a] -> yield! next [v; a] xs
+     | SeqCons((E, NE v, E), xs), [a; b] -> yield! next [v; b] xs
+     | SeqCons((E, E, NE v), xs), [a; b] -> yield! next [v; a; b] xs
+     | SeqCons((E, E, NE v), xs), [a; b; c] -> yield! next [v; b; c] xs
+     | SeqCons((E, NE v, E), xs), [a; b; c] -> yield! next [v; c] xs
+     | SeqCons((NE v, E, E), xs), [a; b] -> yield! next [v] xs
+     | SeqCons(x, _), p -> printfn "%A %A" x p]
 
+let toConcept(ax : string list) =
+    [match ax with
+     | [x] ->
+         yield owl.individual !(sprintf "qsc:%s" x) [!"skos:Concept"]
+                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string)]
+     | [y; x] ->
+         yield owl.individual !(sprintf "qsc:%s" x) [!"skos:Concept"]
+                   [objectProperty !"skos:broader" !(sprintf "qsc:%s" y)]
+         yield owl.individual !(sprintf "qsc:%s" y) [!"skos:Concept"]
+                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
+                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" x)]
+     | [z; y; x] ->
+         yield owl.individual !(sprintf "qsc:%s" x) [!"skos:Concept"]
+                   [objectProperty !"skos:broader" !(sprintf "qsc:%s" y)]
+         yield owl.individual !(sprintf "qsc:%s" z) [!"skos:Concept"]
+                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
+                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" y)]
+         yield owl.individual !(sprintf "qsc:%s" y) [!"skos:Concept"]
+                   [objectProperty !"skos:broader" !(sprintf "qsc:%s" z)]
+         yield owl.individual !(sprintf "qsc:%s" y) [!"skos:Concept"]
+                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
+                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" x)]
+         yield owl.individual !(sprintf "qsc:%s" z) [!"skos:Concept"]
+                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
+                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" y)]]
 
-let r = toPaths csv.Rows []
-*)
-let toConcept (ax:string list) = [
-  match ax with
-  | [x] ->
-     yield owl.cls !(sprintf "qsc:%A"x) [!"skos:Concept"] [dataProperty !"skos:prefLabel" (x^^xsd.string)]
-  | [y;x] ->
-     yield owl.cls !(sprintf "qsc:%A"x) [!"skos:Concept"] [objectProperty !"skos:broader" !(sprintf "qsc:%A"y)]
-     yield owl.cls !(sprintf "qsc:%A"y) [!"skos:Concept" ] [
-               dataProperty !"skos:prefLabel" (x^^xsd.string)
-               objectProperty !"skos:narrower" !(sprintf "qsc:%A"x)]
-  | [z;y;x] ->
-     yield owl.cls !(sprintf "qsc:%A"x) [!"skos:Concept"] [objectProperty !"skos:broader" !(sprintf "qsc:%A"z)]
-     yield owl.cls !(sprintf "qsc:%A"z) [!"skos:Concept"] [
-                 dataProperty !"skos:prefLabel" (x^^xsd.string)
-                 objectProperty !"skos:narrower" !(sprintf "qsc:%A"y)
-                ]
-     yield owl.cls !(sprintf "qsc:%A"x) [!"skos:Concept"] [objectProperty !"skos:broader" !(sprintf "qsc:%A"y)]
-     yield owl.cls !(sprintf "qsc:%A"y) [!"skos:Concept"] [
-                 dataProperty !"skos:prefLabel" (x^^xsd.string)
-                 objectProperty !"skos:narrower" !(sprintf "qsc:%A"x)
-                ]
-     yield owl.cls !(sprintf "qsc:%A"z) [!"skos:Concept"] [
-                 dataProperty !"skos:prefLabel" (x^^xsd.string)
-                 objectProperty !"skos:narrower" !(sprintf "qsc:%A"y)
-                ]
-  ]
-
-let g = graph.empty (!"http://nice.org.uk/ns/qualitystandard" ) []
+let g = graph.empty (!"http://nice.org.uk/ns/qualitystandard") []
+let csv = loadCsv ""
 toPaths csv.Rows []
 |> List.collect toConcept
-|> output.toGraph g
-|> output.format graph.write.ttl (graph.toFile "")
+|> Assert.resources g
+|> graph.format graph.write.ttl (graph.toFile "")
+|> ignore
