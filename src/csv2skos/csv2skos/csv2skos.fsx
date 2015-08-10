cf9c3f9d39c0acdc1469dbfa75f3bec15028d4ee
Nathan Smith <nathan@smeef.co.uk>
2015-08-10 11:58:33 +0100

    generate owl from csv

--- src/csv2skos/csv2skos/csv2skos.fsx
+++ src/csv2skos/csv2skos/csv2skos.fsx
@@ -1,6 +1,7 @@
 #I "../../../packages"
 #r "../../../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
-#r "../../../packages/FSharp.RDF/lib/net40/FSharp.RDF.dll"
+#I "../../../packages/FSharp.RDF/lib/"
+#r "../../../packages/FSharp.RDF/lib/FSharp.RDF.dll"
 #r "../../../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r "../../../packages/VDS.Common/lib/net40-client/VDS.Common.dll"
 #r "../../../packages/dotNetRDF/lib/net40/dotNetRDF.dll"
@@ -12,80 +13,82 @@ open Assertion
 open graph
 open rdf
 
+let (++) a b = System.IO.Path.Combine(a,b)
 
-let mkTuple (r:CsvRow) =
-  let c = r.Columns.Length
-  match c with
-    | 1 -> (r.[0], "", "")
-    | 3 -> (r.[0], r.[1], r.[2])
-    | _ -> ("","","")
+let csv = FSharp.Data.CsvFile.Load(__SOURCE_DIRECTORY__ ++ "../csv2skos/sample.csv")
 
-
-let loadCsv(p : string) = CsvFile.Load(p)
-
-let rSpaces(x : string * string * string) =
-    match x with
-    | (x, y, z) ->
-        (x.Replace(' ', '-'), y.Replace(' ', '-'), z.Replace(' ', '-'))
+let rec depthTuple c xs =
+  match xs with
+    | ""::xs -> depthTuple (c+1) xs
+    | x::_ -> (c,x)
 
 
-let (|SeqEmpty|SeqCons|)(xs : 'a seq) =
-    if Seq.isEmpty xs then SeqEmpty
-    else SeqCons(Seq.head xs |> mkTuple, Seq.skip 1 xs)
+type context = { parent: int * string
+                 previous: int * string
+                 grandparents: (int * string) list
+                 }
 
-let (|E|NE|) s =
-    if System.String.IsNullOrEmpty s then E
-    else NE s
+let createOwlResource label parentLabel =
+    owl.cls !!("http://ld.nice.org.uk/qualitystandards/conditionanddisease#" + label) [
+    !!("http://ld.nice.org.uk/qualitystandards#" + parentLabel)
+    ] [
+    dataProperty !!"rdfs:label" (label ^^xsd.string)
+    ]
 
-let rec toPaths xs p =
-    [let next p xs = p :: (toPaths xs p)
-     match xs, p with
-     | SeqEmpty, _ -> ()
-     | SeqCons((NE v, E, E), xs), [] -> yield! next [v] xs
-     | SeqCons((NE v, E, E), xs), [a] -> yield! next [v] xs
-     | SeqCons((E, NE v, E), xs), [a] -> yield! next [v; a] xs
-     | SeqCons((E, NE v, E), xs), [a; b] -> yield! next [v; b] xs
-     | SeqCons((E, E, NE v), xs), [a; b] -> yield! next [v; a; b] xs
-     | SeqCons((E, E, NE v), xs), [a; b; c] -> yield! next [v; b; c] xs
-     | SeqCons((E, NE v, E), xs), [a; b; c] -> yield! next [v; c] xs
-     | SeqCons((NE v, E, E), xs), [a; b] -> yield! next [v] xs
-     | SeqCons(x, _), p -> printfn "%A %A" x p]
+let moveParent (context:context) tuples  =
+  match context, tuples with
+    | _,[] -> context
+    | { parent= (parentDepth,_); previous= previous; grandparents=grandparents}, (depth, label)::_ ->
+      printfn "d %d" ( depth-parentDepth )
+      match depth-parentDepth with
+        | 2 -> {context with
+                    parent = previous
+                    previous = (depth,label)
+                    grandparents= context.parent::grandparents}
+        | 0 -> { context with
+                    parent= List.head grandparents
+                    previous = (depth,label)
+                    grandparents= List.tail grandparents}
+        | _ -> {context with previous = (depth,label)}
 
-let toConcept(ax : string list) =
-    [match ax with
-     | [x] ->
-         yield owl.individual !(sprintf "qsc:%s" x) [!"skos:Concept"]
-                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string)]
-     | [y; x] ->
-         yield owl.individual !(sprintf "qsc:%s" x) [!"skos:Concept"]
-                   [objectProperty !"skos:broader" !(sprintf "qsc:%s" y)]
-         yield owl.individual !(sprintf "qsc:%s" y) [!"skos:Concept"]
-                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
-                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" x)]
-     | [z; y; x] ->
-         yield owl.individual !(sprintf "qsc:%s" x) [!"skos:Concept"]
-                   [objectProperty !"skos:broader" !(sprintf "qsc:%s" y)]
-         yield owl.individual !(sprintf "qsc:%s" z) [!"skos:Concept"]
-                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
-                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" y)]
-         yield owl.individual !(sprintf "qsc:%s" y) [!"skos:Concept"]
-                   [objectProperty !"skos:broader" !(sprintf "qsc:%s" z)]
-         yield owl.individual !(sprintf "qsc:%s" y) [!"skos:Concept"]
-                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
-                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" x)]
-         yield owl.individual !(sprintf "qsc:%s" z) [!"skos:Concept"]
-                   [dataProperty !"skos:prefLabel" (x ^^ xsd.string);
-                    objectProperty !"skos:narrower" !(sprintf "qsc:%s" y)]]
+let rec owlGen (context:context) tuples =
+  match tuples with
+  | [] -> []
+  | (depth, label)::tail ->
+    let context' = moveParent context tuples
+    createOwlResource label (snd context'.parent) :: owlGen (moveParent context tuples) tail
+
+let newContext a = {
+  parent=a
+  previous=a
+  grandparents= []
+     }
 
 
+let g = Graph.empty !!"http://test" []
+let sb = System.Text.StringBuilder()
+csv.Rows
+|> Seq.map (fun a -> a.Columns)
+|> Seq.map Array.toList
+|> Seq.map (depthTuple 1)
+|> Seq.toList
+|> owlGen (newContext (0, "0-0"))
+|> Assert.graph g
+|> Graph.writeTtl (toString sb)
 
-let csv = loadCsv "csv file path"
-for i in csv.Rows do
-  printfn "%A" (mkTuple i)
 
-let g = graph.empty (!"http://nice.org.uk/ns/qualitystandard") []
-toPaths csv.Rows []
-|> List.collect toConcept
-|> Assert.resources g
-|> graph.format graph.write.ttl (graph.toFile "C:\\test.ttl")
-|> ignore
+//test cases
+let context = newContext (0, "0-0")
+
+let context' = moveParent context [(1, "1-1")]
+let test1 = context' = {parent=(0, "0-0"); previous=(1, "1-1");grandparents=[]}
+
+let context'' = moveParent context' [(2, "2-1")]
+let test2 = context'' = {parent=(1, "1-1"); previous=(2, "2-1");grandparents=[(0, "0-0")]}
+
+let context''' = moveParent context'' [(2, "2-2")]
+let test3 = context''' = { parent=(1,"1-1"); previous=(2, "2-2"); grandparents=[(0,"0-0")]}
+
+let context'''' = moveParent context''' [1,("1-2")]
+let test4 = context'''' = { parent=(0,"0-0"); previous=(1,"1-2"); grandparents=[]}
+
